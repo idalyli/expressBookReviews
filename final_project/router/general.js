d10aedb2299fd94a078c289729597245d19db185
Idaly Diaz <67562162+idalyli@users.noreply.github.com>
2025-09-16 15:18:42 -0500

    Added task5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,8 +67,16 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let book=books[isbn]
+  if (book){
+  res.send(book.reviews)
+  }
+  else{
+  return res.status(300).json({message: `Not exist a book with ISBN equal ${isbn} `});
+
+ 
+  }
 });
 
 module.exports.general = public_users;
